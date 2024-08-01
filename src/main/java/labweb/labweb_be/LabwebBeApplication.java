0538730d12f577eb5b9c5f645781afa3b18bb753
injae-348 <ant0593@naver.com>
2024-08-01 16:04:10 +0900

    feat: create spring project

---
+++ src/main/java/labweb/labweb_be/LabwebBeApplication.java
@@ -0,0 +1,13 @@
+package labweb.labweb_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabwebBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabwebBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/labweb/labweb_be/LabwebBeApplicationTests.java
@@ -0,0 +1,13 @@
+package labweb.labweb_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabwebBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
