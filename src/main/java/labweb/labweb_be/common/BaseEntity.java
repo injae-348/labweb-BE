e13e7173ad78eb75196d3a12372323d1691a3842
injae-348 <ant0593@naver.com>
2024-08-03 23:25:07 +0900

    feat: BaseEntity 포멧 지정

--- src/main/java/labweb/labweb_be/common/BaseEntity.java
+++ src/main/java/labweb/labweb_be/common/BaseEntity.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -17,8 +18,10 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
     private LocalDateTime updatedAt;
 }
