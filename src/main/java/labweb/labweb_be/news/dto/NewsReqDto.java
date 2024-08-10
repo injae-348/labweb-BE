d52ff3ad3801610f5aa834cf5855c56d22694590
injae-348 <ant0593@naver.com>
2024-08-10 14:39:38 +0900

    feat: Dto -> Entity 변경하는 toEntity 추가

--- src/main/java/labweb/labweb_be/news/dto/NewsReqDto.java
+++ src/main/java/labweb/labweb_be/news/dto/NewsReqDto.java
@@ -1,8 +1,10 @@
 package labweb.labweb_be.news.dto;
 
-import labweb.labweb_be.common.image.domain.Image;
-import labweb.labweb_be.common.image.dto.ImageReqDto;
+import labweb.labweb_be.image.domain.Image;
+import labweb.labweb_be.image.dto.ImageReqDto;
+import labweb.labweb_be.news.domain.News;
 import lombok.Builder;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,7 +13,15 @@ import java.util.List;
 public record NewsReqDto(
         LocalDateTime date,
         String activity,
-        String content,
-        List<ImageReqDto> images
+        String content
 ) {
+
+    public News toEntity(List<Image> images) {
+        return News.builder()
+                .date(date)
+                .activity(activity)
+                .content(content)
+                .images(images)
+                .build();
+    }
 }
